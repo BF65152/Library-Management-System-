a864a1152a255a901a7ace7d06202105e31ecbc9
yairzawi770 <148704993+yairzawi770@users.noreply.github.com>
2025-11-10 11:59:43 +0200

    finish

---
+++ library.py
@@ -0,0 +1,11 @@
+class Library:
+    def __init__(self, list_of_books, list_of_users):
+        self.list_of_books = []
+        self.list_of_users = list_of_users
+
+    def add_book(self, book):
+        pass
+
+
+
+
