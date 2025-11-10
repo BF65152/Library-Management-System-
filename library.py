d389a7f031023df25e701f82d399aa2972359570
yairzawi770 <148704993+yairzawi770@users.noreply.github.com>
2025-11-10 15:22:51 +0200

    .

--- library.py
+++ library.py
@@ -1,10 +1,11 @@
 class Library:
     def __init__(self, list_of_books, list_of_users):
         self.list_of_books = []
-        self.list_of_users = list_of_users
+        self.list_of_users = []
 
     def add_book(self, book):
-        pass
+        self.list_of_books.append(book)
+        return
 
     def add_user(self, user):
         self.list_of_users.append(user)
