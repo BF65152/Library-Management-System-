f3be8a54ea2f7e558e22808973607c017b5be81a
chaim <chaimfrazan@gmail.com>
2025-11-11 14:49:33 +0200

    the finish kod

--- library.py
+++ library.py
@@ -1,3 +1,4 @@
+import user
 from user import User
 from book import Book
 
@@ -29,29 +30,43 @@ class Library:
 #אחרי שמוצאים את המשתמש בודקים עם הספר קיים ואז משאילים
     def borrow_book(self,book_isbn,user_id):
         user = self.find_user_by_id(user_id)
+        if not user:
+            print('user not found')
+            return False
         for book in self.list_of_books:
             if book.isbn == book_isbn:
-                print("your borrowed the book successfully")
-                user.borrowed_books.append(book_isbn)
-                book.is_available = False
+                if book.is_available:
+                    print("your borrowed the book successfully")
+                    user.borrowed_books.append(book_isbn)
+                    book.is_available = False
+                    return
+                print("the book already borrowed")
+                return
+        print("the book is not exist")
+        return
 
 
 
 #החזרת ספר עי בדיקה האם הושאל בכלל
     def return_book(self, user_id, book_isbn):
         user = self.find_user_by_id(user_id)
+        if not isinstance(user, User):
+            print("user not found")
+            return False
+
         for book in self.list_of_books:
-            if book.is_available and book_isbn == book.isbn:
-                print("the book is not borrowed")
-            book.is_available = True
+            if book.isbn == book_isbn:
+                if book.is_available:
+                    print("the book was not borrowed")
+                    return False
+                book.is_available = True
+                if book_isbn in user.borrowed_books:
+                    user.borrowed_books.remove(book_isbn)
+                print("book returned successfully")
+                return
+        print("the book does not exist")
 
-            #מסיר את הספר שהוחזר מהרשימה של הספרים המושאלים
-            for u in user.borrowed_books:
-                if u == book_isbn:
-                    user.borrowed_books.remove(u)
-
-
-#רשימה של ספרים שלא הושאלו
+    #רשימה של ספרים שלא הושאלו
     def list_available_books(self):
         for book in self.list_of_books:
             if book.is_available:
@@ -64,6 +79,6 @@ class Library:
         for book in self.list_of_books:
             if title == book.title:
                 print("the book exist")
-            print("the book is not exist")
-
+                return
+        print("the book is not exist")
 
